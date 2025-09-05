dd5972a210876754fe74b2c03024bc558f6d0094
Pozdro70 <mateusz_wit@wp.pl>
2025-09-05 17:03:13 +0200

    Dodaj pliki projektów.

---
+++ MapManager.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+//Typ pomieszczenia
+enum RoomType {
+	IRoom,
+	TRoom,
+	LRoom,
+	endRoom,
+	XRoom,
+	SpecialRoom
+};
+
+//Klasa pomieszczenia
+class Room {
+public:
+	int id;
+	std::string name;
+	std::string fname;
+	RoomType roomType;
+};
+
+
+//Glowna klasa MapManager
+#pragma once
+class MapManager
+{
+public:
+	void loadMapRoomFiles() {
+
+	}
+};
