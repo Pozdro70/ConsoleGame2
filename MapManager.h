1c35e3604c8c86a1811583747720b63ab3ac450c
Pozdro70 <mateusz_wit@wp.pl>
2025-09-06 23:07:39 +0200

    final day update - started coding map gen

--- MapManager.h
+++ MapManager.h
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <random>
 #include "ConsoleUtils.h"
+#include <iostream>
+#include <fstream>  
+#include <vector>
+#include <string>
+#include <stack>
 
 //funkcja debugujaca wyswietlajaca wszystkie wczytane pomieszczenia
 static void dispAllRooms() {
@@ -16,6 +21,21 @@ static void dispAllRooms() {
 		}
 	}
 }
+
+class Map {
+public:
+	std::string saveName;
+	std::string gameVersion;
+	std::vector<std::vector<char>> mapImage;
+	int sizeX;
+	int sizeY;
+};
+
+class MapRoomCell {
+	bool visited;
+	Room room;
+	RoomType roomType;
+};
 
 #pragma once
 class MapManager
@@ -38,13 +58,29 @@ public:
 		std::vector<Room> rooms;
 		rooms = roomManager.loadMapRooms();
 
+		/*
 		//Stawianie pomieszczenia startowego
 
 		for (Room room : rooms) {
 			if (room.roomType == RoomType::endRoom && room.exitRotations.front() == RoomExitRotation::East && room.passages==1) {
-				placeRoom(room, 0, getConsoleHeight()/2);
+				placeRoom(room, 0, getConsoleHeight()/3);
 			}
 		}
+		*/
+
+		//Inicjalizacja mapy
+		std::vector<std::string> mapFile;
+		Map map;
+
+		map.saveName = "NewMap1"; // TODO: naming not implemented yet
+		map.gameVersion = "0.0.1-InDev"; // TODO: versioning not implemented yet
+		map.sizeX = 100;
+		map.sizeY = 100;
+
+
+		std::vector<std::vector<MapRoomCell>> mapCells;
+		std::stack<MapRoomCell> visitedMapCells;
+		
 	}
 };
 
